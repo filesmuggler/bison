dacdc2fe6feb443e1538917eed536de258a88752
filesmuggler <krzste09@outlook.com>
2018-12-29 18:19:28 +0100

    progress

--- arm.js
+++ arm.js
@@ -4,36 +4,38 @@ render();
 /**
  * Variables to hold information about the viewer
  */
-var camera, scene, renderer, parent, pivot, controls;
-
-var deg90 = 90*Math.PI/180;
+var camera, scene, renderer, controls;
+var arm_group, joint1_group, joint2_group, joint3_group;
 
 /**
  * Variables to hold objects for the arm
  */
 var base, link1, joint1, link2, joint2, link3, joint3, end_effector;
 
-//for testing
-var group;
-var startTime = Date.now();
 
 /**
  * init() is for set up of environment
  */
 function init() {
-    
-    var canvas = document.getElementById('robot-presentation');
+
+    //var canvas = document.getElementById('robot-presentation');
     /*
     renderer = new THREE.WebGLRenderer({ canvas: canvas });
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
     renderer.setViewport(0, 0, canvas.clientWidth, canvas.clientHeight);
     */
+
+    /*
     renderer = new THREE.WebGLRenderer();
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
-    renderer.setSize( canvas.width, canvas.height );
-    document.getElementById('robot-presentation').appendChild( renderer.domElement );
+    renderer.setSize(canvas.width, canvas.height);
+    document.getElementById('robot-presentation').appendChild(renderer.domElement);*/
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
     /*
     scene = new THREE.Scene();
@@ -42,20 +44,28 @@ function init() {
     */
 
     // scene
-	scene = new THREE.Scene();
-	
-	// camera
-	camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 0.1, 100 );
-	camera.position.set( 15, 15, 15 );
+    scene = new THREE.Scene();
 
-	// controls
-	controls = new THREE.OrbitControls( camera );
-	
-	// axes
-	scene.add( new THREE.AxisHelper( 2 ) );
+    // camera
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 100);
+    camera.position.set(15, 15, 15);
+    //scene.add(camera);
 
-    var light = new THREE.DirectionalLight(0xffffff, 0.55);
-    light.position.set(0, 0, 1);
+    // controls
+    controls = new THREE.OrbitControls(camera);
+    controls.enableKeys = true;
+    controls.keys = {
+        LEFT: 37, //left arrow
+        UP: 38, // up arrow
+        RIGHT: 39, // right arrow
+        BOTTOM: 40 // down arrow
+    }
+
+    // axes
+    scene.add(new THREE.AxisHelper(2));
+
+    var light = new THREE.DirectionalLight(0xffffff, 1.0);
+    light.position.set(10, 10, 10);
     scene.add(light);
 
     placeObjects();
@@ -68,56 +78,106 @@ function placeObjects() {
 
     var box = new THREE.BoxGeometry(1, 1, 1);
     var cylinder = new THREE.CylinderGeometry(0.5, 0.5, 1, 64);
-    var sphere = new THREE.SphereGeometry(1,32,32);
+    var sphere = new THREE.SphereGeometry(1, 32, 32);
 
-    var base_material = new THREE.MeshBasicMaterial({ color: 0x767674 });
-    var link_material = new THREE.MeshBasicMaterial({ color: 0x434341 });
-    var joint_material = new THREE.MeshBasicMaterial({ color: 0x1CB0CC });
-    var end_effector_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    var base_material = new THREE.MeshStandardMaterial({
+        color: 0x767674,
+        roughness: 0.53,
+        metalness: 0.79,
+        emissive: 0x757575,
+        emissiveIntensity: 0.5
+    });
+    var link_material = new THREE.MeshStandardMaterial({
+        color: 0x434341,
+        roughness: 0.53,
+        metalness: 0.79,
+        emissive: 0x757575,
+        emissiveIntensity: 0.5
+    });
+    var joint_material = new THREE.MeshStandardMaterial({
+        color: 0x1CB0CC,
+        roughness: 0.53,
+        metalness: 0.79,
+        emissive: 0x757575,
+        emissiveIntensity: 0.5
+    });
+    var end_effector_material = new THREE.MeshStandardMaterial({
+        color: 0xffffff, 
+        roughness: 0.53,
+        metalness: 0.79,
+        emissive: 0x757575,
+        emissiveIntensity: 0.5
+    });
 
-    base = new THREE.Mesh(box,base_material);
-    base.position.set(0,0,0);
-    base.scale.set(3,0.1,3);
-    scene.add(base);
+
+
+    base = new THREE.Mesh(box, base_material);
+    base.position.set(0, 0, 0);
+    base.scale.set(3, 0.1, 3);
+    //scene.add(base);
 
     link1 = new THREE.Mesh(cylinder, link_material);
-    link1.position.set(0,1.5,0);
-    link1.scale.set(0.5,3,0.5);
-    scene.add(link1);
+    link1.position.set(0, 1.5, 0);
+    link1.scale.set(0.5, 3, 0.5);
+    //scene.add(link1);
 
     joint1 = new THREE.Mesh(cylinder, joint_material);
-    joint1.position.set(0,3,0);
-    joint1.scale.set(1,1,1);
-    scene.add(joint1);
+    joint1.position.set(0, 3, 0);
+    joint1.scale.set(1, 1, 1);
+    //scene.add(joint1);
 
     link2 = new THREE.Mesh(cylinder, link_material);
-    link2.position.set(1.5,3,0);
-    link2.rotation.set(0,0,90*Math.PI/180);
-    link2.scale.set(0.5,3,0.5);
-    scene.add(link2);
-    
+    link2.position.set(1.5, 3, 0);
+    link2.rotation.set(0, 0, 90 * Math.PI / 180);
+    link2.scale.set(0.5, 3, 0.5);
+    //scene.add(link2);
+
     joint2 = new THREE.Mesh(cylinder, joint_material);
-    joint2.position.set(3,3,-0.5);
-    joint2.scale.set(1,2,1);
-    joint2.rotation.set(0,90*Math.PI/180,90*Math.PI/180);
-    scene.add(joint2);
+    joint2.position.set(3, 3, -0.5);
+    joint2.scale.set(1, 2, 1);
+    joint2.rotation.set(0, 90 * Math.PI / 180, 90 * Math.PI / 180);
+    //scene.add(joint2);
 
     link3 = new THREE.Mesh(cylinder, link_material);
-    link3.position.set(4.5,3,-1);
-    link3.rotation.set(0,0,90*Math.PI/180);
-    link3.scale.set(0.5,3,0.5);
-    scene.add(link3);
+    link3.position.set(4.5, 3, -1);
+    link3.rotation.set(0, 0, 90 * Math.PI / 180);
+    link3.scale.set(0.5, 3, 0.5);
+    //scene.add(link3);
 
     joint3 = new THREE.Mesh(box, joint_material);
-    joint3.position.set(6,3,-1);
-    joint3.rotation.set(0,0,0);
-    joint3.scale.set(0.7,0.7,0.7);
-    scene.add(joint3);
+    joint3.position.set(6, 3, -1);
+    joint3.rotation.set(0, 0, 0);
+    joint3.scale.set(0.7, 0.7, 0.7);
+    //scene.add(joint3);
 
     end_effector = new THREE.Mesh(sphere, end_effector_material);
-    end_effector.position.set(6.5,3,-1);
-    end_effector.scale.set(0.2,0.2,0.2);
-    scene.add(end_effector);
+    end_effector.position.set(6.5, 3, -1);
+    end_effector.scale.set(0.2, 0.2, 0.2);
+
+    //adding the groups and objects to the scene
+    arm_group = new THREE.Object3D();
+    joint1_group = new THREE.Object3D();
+    joint2_group = new THREE.Object3D();
+    joint3_group = new THREE.Object3D();
+
+
+    joint3_group.add(end_effector);
+    joint3_group.add(joint3);
+
+    joint2_group.add(joint2);
+    joint2_group.add(link3);
+    joint2_group.add(joint3_group);
+
+    joint1_group.add(link2);
+    joint1_group.add(joint1)
+    joint1_group.add(joint2_group);
+
+    arm_group.add(link1);
+    arm_group.add(base);
+    arm_group.add(joint1_group);
+
+    scene.add(arm_group);
+    //scene.add(end_effector);
     /*
     var cubeA = new THREE.Mesh(geometry, material1);
     cubeA.position.set(1, 1, 0);
@@ -131,7 +191,7 @@ function placeObjects() {
     group.add(cubeA);
     group.add(cubeB);
     scene.add(group);*/
-    
+
 }
 
 /**
@@ -159,22 +219,6 @@ function render() {
     requestAnimationFrame(render);
     // actually display the scene in the Dom element
     renderer.render(scene, camera);
+    controls.update();
 }
 
-/**
- * moveArm() sets parameters for the joints in direct kinematics
- */
-function moveArm() {
-    theta1 = document.getElementById("theta1").value;
-    var n_vector = THREE.Vector3();
-    console.log(joint1.position);
-    console.log(link1.position);
-    var rot_vector = new THREE.Vector3();
-    rot_vector.x = joint1.position.x - link1.position.x;
-    rot_vector.y = joint1.position.y - link1.position.y;
-    rot_vector.z = joint1.position.z - link1.position.z;
-    console.log(rot_vector);
-    link2.rotateAroundWorldAxis(link1.position,theta1*Math.PI/180);
-    theta2 = document.getElementById("theta2").value;
-    d3 = document.getElementById("d3").value;
-}
--- other.js
+++ other.js
@@ -6,3 +6,15 @@ function getOption() {
     document.getElementById("robot-current-mode").innerHTML =
         obj.options[obj.selectedIndex].text;
 }
+
+/**
+ * moveArm() sets parameters for the joints in direct kinematics
+ */
+function moveArm() {
+    theta1 = document.getElementById("theta1").value;
+    console.log(theta1);
+    theta2 = document.getElementById("theta2").value;
+    console.log(theta2);
+    d3 = document.getElementById("d3").value;
+    console.log(d3);
+}
