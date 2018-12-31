70acd20f689baa8b6a7588109e718a78ba2eadda
filesmuggler <krzste09@outlook.com>
2018-12-31 11:14:19 +0100

    removed comments

--- arm.js
+++ arm.js
@@ -31,31 +31,9 @@ var AXIS_jg1 = new THREE.Vector3( 0,0,1 ).normalize();
  * init() is for set up of environment
  */
 function init() {
-
-    //var canvas = document.getElementById('robot-presentation');
-    /*
-    renderer = new THREE.WebGLRenderer({ canvas: canvas });
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
-    renderer.setViewport(0, 0, canvas.clientWidth, canvas.clientHeight);
-    */
-
-    /*
-    renderer = new THREE.WebGLRenderer();
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
-    renderer.setSize(canvas.width, canvas.height);
-    document.getElementById('robot-presentation').appendChild(renderer.domElement);*/
-
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-
-    /*
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
-    camera.position.z = 5;
-    */
 
     // scene
     scene = new THREE.Scene();
@@ -63,7 +41,6 @@ function init() {
     // camera
     camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 100);
     camera.position.set(10, 10, 10);
-    //scene.add(camera);
 
     // controls
     controls = new THREE.OrbitControls(camera);
@@ -89,8 +66,6 @@ function init() {
     var light_down_2 = new THREE.PointLight(0xffffff, 1.0, 100);
     light_down_2.position.set(-10, 0, -10);
     scene.add(light_down_2);
-
-    
 
     placeObjects();
 }
@@ -133,49 +108,37 @@ function placeObjects() {
         emissiveIntensity: 0.5
     });
 
-
-
     base = new THREE.Mesh(box, base_material);
     base.position.set(0, 0, 0);
     base.scale.set(3, 0.1, 3);
-    //scene.add(base);
 
     link1 = new THREE.Mesh(cylinder, link_material);
     link1.position.set(0, 1.5, 0);
     link1.scale.set(0.5, 3, 0.5);
-    //scene.add(link1);
 
     joint1 = new THREE.Mesh(cylinder, joint_material);
     joint1.position.set(0, 3, 0);
     joint1.scale.set(1, 1, 1);
-    //scene.add(joint1);
 
     link2 = new THREE.Mesh(cylinder, link_material);
     link2.position.set(1.5, 3, 0);
     link2.rotation.set(0, 0, 90 * Math.PI / 180);
     link2.scale.set(0.5, 3, 0.5);
-    //scene.add(link2);
 
     joint2 = new THREE.Mesh(cylinder, joint_material);
     joint2.position.set(3, 3, -0.5);
     joint2.scale.set(1, 2, 1);
     joint2.rotation.set(0, 90 * Math.PI / 180, 90 * Math.PI / 180);
-    //scene.add(joint2);
 
     link3 = new THREE.Mesh(cylinder, link_material);
     link3.position.set(4.5, 3, -1);
     link3.rotation.set(0, 0, 90 * Math.PI / 180);
     link3.scale.set(0.5, 3, 0.5);
 
-    
-    //scene.add(link3);
-
     joint3 = new THREE.Mesh(box, joint_material);
     joint3.position.set(6, 3, -1);
     joint3.rotation.set(0, 0, 0);
     joint3.scale.set(0.7, 0.7, 0.7);
-    //scene.add(joint3);
-
 
     end_effector = new THREE.Mesh(sphere, end_effector_material);
     end_effector.position.set(6.5, 3, -1);
@@ -187,7 +150,6 @@ function placeObjects() {
     end_effector_conn.translateY(1.70);
     end_effector_conn.scale.set(0.25,3,0.25);
     
-
     //adding the groups and objects to the scene
     arm_group = new THREE.Object3D();
     joint1_group = new THREE.Object3D();
@@ -205,6 +167,8 @@ function placeObjects() {
 
     joint2_group.add(joint3);
     joint2_group.add(link3);
+
+   
     
     
 
