c54b563a6e2b2c8d1f1016a4a2e03c250889ee19
filesmuggler <krzste09@outlook.com>
2019-01-01 18:06:56 +0100

    getting started again

--- arm.js
+++ arm.js
@@ -6,6 +6,8 @@ render();
  */
 var camera, scene, renderer, controls;
 
+var parent, pivot;
+
 /**
  * deprecated solution
  */
@@ -28,7 +30,7 @@ var workingMode, parameter_1, parameter_2, parameter_3;
 var theta1, theta2, d3;
 var px, py, pz;
 
-//var AXIS_jg1 = new THREE.Vector3( 0,1,0 ).normalize();
+var AXIS_jg1 = new THREE.Vector3( 0,1,0 ).normalize();
 
 
 /**
@@ -39,25 +41,12 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    // scene
     scene = new THREE.Scene();
 
-    // camera
     camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 100);
     camera.position.set(10, 10, 10);
 
-    // controls
     controls = new THREE.OrbitControls(camera);
-    controls.enableKeys = true;
-    controls.keys = {
-        LEFT: 37, //left arrow
-        UP: 38, // up arrow
-        RIGHT: 39, // right arrow
-        BOTTOM: 40 // down arrow
-    }
-
-    // axes
-    scene.add(new THREE.AxisHelper(2));
 
     var light_up = new THREE.PointLight(0xffffff, 3.0, 100);
     light_up.position.set(0, 10, 0);
@@ -112,50 +101,68 @@ function placeObjects() {
         emissiveIntensity: 0.5
     });
 
+    parent = new THREE.Object3D();
+    scene.add(parent);
+
+    pivot = new THREE.Object3D();
+    parent.add(pivot);
+
     base = new THREE.Mesh(box, base_material);
     base.position.set(0, 0, 0);
     base.scale.set(3, 0.1, 3);
+    parent.add(base);
 
     link1 = new THREE.Mesh(cylinder, link_material);
     link1.position.set(0, 1.5, 0);
     link1.scale.set(0.5, 3, 0.5);
+    parent.add(link1);
 
     joint1 = new THREE.Mesh(cylinder, joint_material);
     joint1.position.set(0, 3, 0);
     joint1.scale.set(1, 1, 1);
+    parent.add(joint1);
 
     link2 = new THREE.Mesh(cylinder, link_material);
     link2.position.set(1.5, 3, 0);
     link2.rotation.set(0, 0, 90 * Math.PI / 180);
     link2.scale.set(0.5, 3, 0.5);
+    
+    parent.add(link2);
 
     joint2 = new THREE.Mesh(cylinder, joint_material);
-    joint2.position.set(3, 3, -0.5);
+    joint2.position.set(3.25,3,-0.3);
     joint2.scale.set(1, 2, 1);
     joint2.rotation.set(0, 90 * Math.PI / 180, 90 * Math.PI / 180);
     joint2.add( new THREE.AxisHelper( 2.5 ) );
-    //scene.add(joint2);
 
+    parent.add(joint2);
+
+    /*
     link3 = new THREE.Mesh(cylinder, link_material);
-    link3.position.set(4.5, 3, -1);
+    //link3.position.set(4.5, 3, -1);
     link3.rotation.set(0, 0, 90 * Math.PI / 180);
     link3.scale.set(0.5, 3, 0.5);
+    link3.add( new THREE.AxisHelper( 2.5 ) );
 
+    joint2.add(link3);
+
+    /*
     joint3 = new THREE.Mesh(box, joint_material);
     joint3.position.set(6, 3, -1);
     joint3.rotation.set(0, 0, 0);
-    joint3.scale.set(0.7, 0.7, 0.7);
+    //joint3.scale.set(0.7, 0.7, 0.7);
 
     end_effector = new THREE.Mesh(sphere, end_effector_material);
     end_effector.position.set(6.5, 3, -1);
-    end_effector.scale.set(0.25, 0.25, 0.25);
+    //end_effector.scale.set(0.25, 0.25, 0.25);
 
     end_effector_conn = new THREE.Mesh(cylinder, link_material);
     end_effector_conn.position.set(6.5, 3, -1);
     end_effector_conn.rotation.set(0, 0, 90 * Math.PI / 180);
     end_effector_conn.translateY(1.70);
-    end_effector_conn.scale.set(0.25,3,0.25);
+    //end_effector_conn.scale.set(0.25,3,0.25);
     
+    /*
     //adding the groups and objects to the scene
     arm_group = new THREE.Object3D();
     joint1_group = new THREE.Object3D();
@@ -174,7 +181,7 @@ function placeObjects() {
 
     joint2_group.add(joint3);
     joint2_group.add(link3);
-    */
+    
     
     link3.add(joint3);
     link3.add(joint3_group);
@@ -192,37 +199,10 @@ function placeObjects() {
     arm_group.add(joint1);
     arm_group.add(base);
     arm_group.add(joint1_group);
-
-    scene.add(arm_group);
+    
+    scene.add(arm_group);*/
     //scene.add(end_effector);
-    /*
-    var cubeA = new THREE.Mesh(geometry, material1);
-    cubeA.position.set(1, 1, 0);
-
-    var cubeB = new THREE.Mesh(geometry, material2);
-    cubeB.position.set(-1, -1, 0);
-    */
-    //create a group and add the two cubes
-    //These cubes can now be rotated / scaled etc as a group
-    /*group = new THREE.Group();
-    group.add(cubeA);
-    group.add(cubeB);
-    scene.add(group);*/
-
-}
-
-/**
- * animate() animates and display the scene
- *
-function animate() {
-    render();
-    requestAnimationFrame(animate);
-}*/
-/*
-window.addEventListener('resize', function(){
-    camera.aspect = canvas.clientWidth/canvas.clientHeight;
-    camera.updateProjectionMatrix();
-});*/
+    }
 
 /**
  * render() renders the 3D scene
@@ -242,14 +222,13 @@ function render() {
      * movement control
      */
     if(workingMode==="Forward"){
-        joint1_group.rotation.set(0,theta1*Math.PI/180,0);
-        //joint2_group.rotation.set(0,0,theta2*Math.PI/180);
-        end_effector_group.position.set(d3,0,0);
-        //joint2_group.rotation.set(0,0,theta2*Math.PI/180);
-        //workingMode=" ";
-        //joint2.rotateOnAxis(AXIS_jg1,0.001);
-        //joint2.rotateOnAxis(AXIS_jg1,theta2*Math.PI/180,0);
-        link3.rotation.set(0,theta2*Math.PI/180,0);
+        //joint1_group.rotation.set(0,theta1*Math.PI/180,0);
+        //joint1.rotateOnAxis(AXIS_jg1,theta1*Math.PI/180);
+        joint1.rotation.set(0,theta1*Math.PI/180,0);
+        //joint2.rotation.set(theta2*Math.PI/180,0,0);
+        //end_effector_group.position.set(d3,0,0);
+        
+        //link3.rotation.set(0,theta2*Math.PI/180,0);
     }
     else if(workingMode==="Inverse"){
         
