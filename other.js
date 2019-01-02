34dfbc2a7c8ca0d55fb015c832d4fac10e0c6a4f
filesmuggler <krzste09@outlook.com>
2019-01-02 22:08:56 +0100

    beta release

--- other.js
+++ other.js
@@ -1,17 +1,61 @@
 /**
+ * initNames()
+ */
+function initNames() {
+    document.getElementById("p1").innerHTML = "Parameter 1";
+    document.getElementById("p2").innerHTML = "Parameter 2";
+    document.getElementById("p3").innerHTML = "Parameter 3";
+}
+
+/**
+ * resetArm()
+ */
+function resetArm(){
+    workingMode = "Forward";
+    theta1 = 0;
+    //console.log(theta1);
+    theta2 = 0;
+    //console.log(theta2);
+    d3 = 0;
+    //console.log(d3);
+    var message = workingMode + " " + theta1 + " " + theta2 + " " + d3;
+    //console.log(message);
+    var iframe = document.getElementById("myIframe");
+    iframe.contentWindow.postMessage(message, '*');
+}
+
+
+
+/**
  * getOption() reads the current mode set by the user
  */
 function getOption() {
     var obj = document.getElementById("robot-control-mode-select");
-    document.getElementById("robot-current-mode").innerHTML =
-        obj.options[obj.selectedIndex].text;
+    var mytext = obj.options[obj.selectedIndex].text;
+    document.getElementById("robot-current-mode").innerHTML = mytext;
+
+    if (mytext === "Forward") {
+        document.getElementById("p1").innerHTML = "Theta1";
+        document.getElementById("p2").innerHTML = "Theta2";
+        document.getElementById("p3").innerHTML = "d3    ";
+    }
+    else if (mytext === "Inverse") {
+        document.getElementById("p1").innerHTML = "Px";
+        document.getElementById("p2").innerHTML = "Py";
+        document.getElementById("p3").innerHTML = "Pz";
+    }
+    else {
+        document.getElementById("p1").innerHTML = "Parameter 1";
+        document.getElementById("p2").innerHTML = "Parameter 2";
+        document.getElementById("p3").innerHTML = "Parameter 3";
+    }
 }
 
 /**
  * moveArm() sets parameters for the joints in direct kinematics
  */
 function getArmParameters() {
-    
+
     workingMode = document.getElementById("robot-current-mode").innerHTML;
     theta1 = document.getElementById("theta1").value;
     //console.log(theta1);
